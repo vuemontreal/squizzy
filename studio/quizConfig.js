9231b2710141bfe27d4dfdd6560627ec87f2bab7
Tommy Lebel <tommy@tomicio.com>
2020-06-11 22:10:38 -0400

    update sanity remoteWebHost

--- studio/quizConfig.js
+++ studio/quizConfig.js
@@ -1,7 +1,7 @@
 export default {
   localWebHost: 'http://localhost:3000', // default Vue port
   localApiHost: 'http://localhost:3000', // default Now API port
-  remoteWebHost: '<your-app-name>.now.sh',
+  remoteWebHost: 'quiz.vuemontreal.org',
   match: {
     correctAnswerScore: 100,
     firstAnswerScore: 50
